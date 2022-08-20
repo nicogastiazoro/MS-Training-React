90bdd0a792d14b97d5911c9279252ceb90000135
nicogastiazoro <gastiazoronicolas@gmail.com>
2022-08-20 15:02:14 -0300

    Commit Clase 3/4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Saludo from './components/Saludo';
 
 function App() {
+
+  const mensaje = 'Bienvenido a mi APP'
+  const estilos = {
+    backgroundColor: '#888',
+    padding: '20px'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hola Mundo</h1>
+      <h3 style={estilos}>{mensaje}</h3>
+      <Saludo name='Juan' lastname='Perez'/>    
     </div>
   );
 }
---
+++ src/components/Saludo.js
@@ -0,0 +1,14 @@
+function Saludo(props){
+    return <h1>Saludos a {props.name} {props.lastname}</h1>
+}
+
+// const Saludo = (props) => {
+//     return <h1>Saludos a {props.name} {props.lastname}</h1>
+// }
+
+// Componente desestructurado
+// function Saludo({name, lastname}){
+//     return <h1>Saludos a {name} {lastname}</h1>
+// }
+
+export default Saludo
